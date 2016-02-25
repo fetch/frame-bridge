e58c94b3e5653eff3b351b6bf5612d00289621d8
Koen Punt <koen@koenpunt.nl>
2016-02-25 11:58:11 +0100

    add "off" method to remove listeners
    add package definition

--- src/frame-bridge.js
+++ src/frame-bridge.js
@@ -1,48 +1,74 @@
-var FrameBridge = (function(){
+var FrameBridge = (function() {
 
   var defaults = {
     targetOrigin: '*'
   };
 
-  var extendObj = function(){
+  var listening = false;
+
+  var listeners = {};
+
+  var objectKeys = Object.keys || function(obj) {
+    var keys = [], prop;
+    for(prop in obj) {
+      if(obj.hasOwnProperty(prop)) keys.push(prop);
+    }
+    return keys;
+  };
+
+  var extendObj = function() {
     var objects = [].slice.call(arguments, 0);
     var i = 0, prop, obj, res = objects.shift();
-    for(; i < objects.length ; ++i ){
+    for(; i < objects.length ; ++i ) {
       obj = objects[i];
-      for(prop in obj){
+      for(prop in obj) {
         if(obj.hasOwnProperty(prop)) res[prop] = obj[prop];
       }
     }
     return res;
   };
 
-  var processEvent = function(listeners, event){
+  var debug = function() {
+    if(!FrameBridge.debug) return;
+    var args = [].slice.call(arguments);
+    args.unshift('[FrameBridge window: "' + window.name + '" title: "' + window.document.title + '"]');
+    console.log.apply(console, args);
+  };
+
+  var processEvent = function(event) {
     try {
       var data = JSON.parse(event.data);
-      if(data.method && listeners[data.method]){
+      debug('processEvent', data.method, data.params);
+      if(data.method && listeners[data.method]) {
         var i = 0, methodListeners = listeners[data.method];
-        for(;i < methodListeners.length; ++i){
+        for(;i < methodListeners.length; ++i) {
           methodListeners[i].call(null, data.params);
         }
       }
-    }catch(e) {
+    } catch(e) {
       ('console' in window) && console.warn(e);
     }
   };
 
-  var FrameBridge = function(options){
-    var _this = this;
+  var startListening = function() {
+    listening = true;
+    debug('startListening');
+    window.addEventListener('message', processEvent, false);
+  };
+
+  var stopListening = function() {
+    listening = false;
+    debug('stopListening');
+    window.removeEventListener('message', processEvent);
+  };
+
+  var FrameBridge = function(options) {
     this.options = extendObj({}, defaults, options || {});
-    window.addEventListener('message', function(event){
-      processEvent(_this.listeners, event);
-    }, false);
   };
 
   extendObj(FrameBridge.prototype, {
 
-    listeners: {},
-
-    trigger: function(method, params, options){
+    trigger: function(method, params, options) {
       options = extendObj({}, this.options, options || {});
       try {
         var target = options.target || parent;
@@ -51,14 +77,33 @@ var FrameBridge = (function(){
           method: method,
           params: params
         }), this.options.targetOrigin);
-      }catch(e){
+      } catch(e) {
         ('console' in window) && console.warn(e);
       }
     },
 
-    on: function(method, callback){
-      this.listeners[method] = (this.listeners[method] || []);
-      this.listeners[method].push(callback);
+    on: function(method, callback) {
+      if(!listening) startListening();
+      listeners[method] = listeners[method] || [];
+      listeners[method].push(callback);
+      return this;
+    },
+
+    off: function(method, callback) {
+      if(listeners[method]) {
+        if(callback) {
+          var index = listeners[method].indexOf(callback);
+          if(~index) {
+            listeners[method].splice(index, 1);
+          }
+        }
+        if(!callback || listeners[method].length === 0) {
+          delete listeners[method];
+        }
+        if(objectKeys(listeners).length === 0) {
+          stopListening();
+        }
+      }
       return this;
     }
 
